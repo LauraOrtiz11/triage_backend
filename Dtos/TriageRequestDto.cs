3c0e58e718f222c86951012574df7f309d328e84
LauraOrtiz11 <ortizlaurava@gmail.com>
2025-10-03 19:18:04 -0500

    HU-05 - Implemetación de IA mejorada, falta enviar signos y sintomas a  la base de Datos

--- Dtos/TriageRequestDto.cs
+++ Dtos/TriageRequestDto.cs
@@ -9,6 +9,6 @@ public class VitalSignsDto
     public int HeartRate { get; set; }
     public int RespiratoryRate { get; set; }
     public string BloodPressure { get; set; } = string.Empty;
-    public decimal Temperature { get; set; }
+    public double Temperature { get; set; }
     public int OxygenSaturation { get; set; }
 }
