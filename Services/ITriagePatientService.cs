bbd68b69ea2498034b7fad3ebdaf9cdd3cf9ac44
LauraOrtiz11 <ortizlaurava@gmail.com>
2025-10-04 20:25:47 -0500

    HU-6 - Listar pacientes funcional y filtra por color, Arreglos en la versión de Cors.

---
+++ Services/ITriagePatientService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using triage_backend.Dtos;
+
+namespace triage_backend.Services
+{
+    public interface ITriagePatientService
+    {
+        List<TriagePatientDto> GetTriagePatients(string? color);
+    }
+}
