dc03f3ea1e8bbd1b4f8c18871f8e3a05c7ffad61
LauraOrtiz11 <ortizlaurava@gmail.com>
2025-10-12 21:35:05 -0500

    HU07 - Registro de resultado final por parte del enfermero funcional

---
+++ Services/TriageResultService.cs
@@ -0,0 +1,24 @@
+﻿using triage_backend.Dtos;
+using triage_backend.Repositories;
+
+namespace triage_backend.Services
+{
+    public class TriageResultService : ITriageResultService
+    {
+        private readonly TriageResultRepository _repository;
+
+        public TriageResultService(TriageResultRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public bool RegisterTriageResult(TriageResultDto result)
+        {
+            // Validación simple
+            if (result.TriageId <= 0 || result.PriorityId <= 0 || result.NurseId <= 0)
+                throw new ArgumentException("All fields are required");
+
+            return _repository.SaveTriageResult(result);
+        }
+    }
+}
