365e8329bc1b05cb7faeefbb49531ec5614a261d
LauraOrtiz11 <ortizlaurava@gmail.com>
2025-10-13 21:27:09 -0500

    HU-12 - Ya se asigna correctamente el medico al respectivo triage, se da inico a la fecha de consulta al igual que se finaliza el triage

---
+++ Services/ConsultationService.cs
@@ -0,0 +1,21 @@
+﻿using triage_backend.Dtos;
+using triage_backend.Repositories;
+using triage_backend.Services;
+
+namespace triage_backend.Services
+{
+    public class ConsultationService : IConsultationService
+    {
+        private readonly ConsultationRepository _repository;
+
+        public ConsultationService(ConsultationRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public bool StartConsultation(StartConsultationDto model)
+        {
+            return _repository.StartConsultation(model);
+        }
+    }
+}
---
+++ Services/IConsultationService.cs
@@ -0,0 +1,9 @@
+﻿using triage_backend.Dtos;
+
+namespace triage_backend.Services
+{
+    public interface IConsultationService
+    {
+        bool StartConsultation(StartConsultationDto model);
+    }
+}
