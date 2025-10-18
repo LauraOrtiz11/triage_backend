9c9659b3198060684eeedc95a6870e27617f1c32
Nicolas Gonzalez <nicolasf.gonzalez1305@gmail.com>
2025-10-17 22:00:00 -0500

    HU - 14 - Busqueda de ID de Historia mediante documento y registro de diagnóstico finalizados y funcionales

---
+++ Dtos/HistoryDiagnosisRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace triage_backend.Dtos
+{
+    public class HistoryDiagnosisRequest
+    {
+        public int HistoryId { get; set; }
+        public int DiagnosisId { get; set; }
+    }
+}
