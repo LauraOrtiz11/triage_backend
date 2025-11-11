34f538359e5f9a766e8ea88389db3d91f330ba98
Nicolas Gonzalez <nicolasf.gonzalez1305@gmail.com>
2025-11-10 21:51:26 -0500

    HU - 21 - Listar Médicos y Enfermeros listo

---
+++ Dtos/UserBasicDto.cs
@@ -0,0 +1,9 @@
+﻿namespace triage_backend.Dtos
+{
+    public class UserBasicDto
+    {
+        public int UserId { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string RoleName { get; set; } = string.Empty;
+    }
+}
